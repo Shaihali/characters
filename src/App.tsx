b43f12edc1dde1b1fbb6709ff89b861241a17295
Shaihali <mr.saybulaev@mail.ru>
2023-05-24 16:22:58 +0300

    feat: add function click button and rendering on the page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
        <div className='wrapper'>
-          <Characters/>
+          <Characters />
        </div>
     </div>
   );
--- src/components/characters/Characters.tsx
+++ src/components/characters/Characters.tsx
@@ -2,27 +2,75 @@ import { useState, useEffect } from "react"
 
 
 
+interface CharacterData {
+  results: Array<{
+    id: number;
+    name: string;
+    image: string;
+  }>;
+	info: {
+		count: number;
+		pages: number;
+	};
+}
+
+
 export function Characters () {
-	const URL = `https://rickandmortyapi.com/api/character/?page=${1}`
-	const [data, setData] = useState({ results: [] })
+	
+	const [data, setData] = useState<CharacterData>({ info: { count: 0, pages: 0 }, results: [] })
+	const [pageNumber, setPageNumber] = useState(1)
+	const URL = getUrl(pageNumber)
 
 	useEffect(() => {
-		async function request() {
-			const responce = await fetch(URL)
-			const data = await responce.json()
-			setData(data)
+		async function fetchData() {
+			const response = await fetch(URL)
+			const responseData = await response.json()
+			setData(responseData)
 		}
-		request()
-	}, [])
-	console.log(data.results[1].image)
+		fetchData()
+	}, [data])
+
+
+	function getUrl (count:number) {
+		const URL = `https://rickandmortyapi.com/api/character/?page=${count}`
+		return URL
+	}
+
+
+
+  function handleNextClick() {
+		if(pageNumber < data.info.pages) {
+			setPageNumber(pageNumber + 1);
+			getUrl(pageNumber + 1);
+		}
+  }
+  function handlePrevClick() {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+      getUrl(pageNumber - 1);
+    }
+  }
+
+
 	return (
 		<>
 			<div className='characters-list'>
-				<div className="card">
-					<img src={data.results[1]} alt="" className="card__img" />
-				</div>
+			{data.results.map((item, i) => (
+          <div className="card" key={i}>
+            <img
+              src={item.image}
+              alt={item.name}
+              className="card__img"
+            />
+						<div className="card__name">{item.name}</div>
+          </div>
+        ))}
 			</div>
-			<div className="pagination"></div>
+			<div className="pagination">
+				<button className="prev button" onClick={handlePrevClick}></button>
+				<div className="current-num">{pageNumber}</div>
+				<button className="next button" onClick={handleNextClick}></button>
+			</div>
 		</>
 	)
 }
