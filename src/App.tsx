ec8077201f31c7c030405e22c2827e35012eff2e
Shaihali <mr.saybulaev@mail.ru>
2023-05-25 17:26:54 +0300

    feat: add component filtr

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Characters } from './components/characters/Characters';
-import { CharacterDetails } from './components/card/CharacterDetails';
+import { CharacterDetails } from './components/characterDetails/CharacterDetails';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { Filtration } from './components/filtration/Filtration';
+import { FilteredCharacters } from './components/FilteredCharacters/FilteredCharacters';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
        <div className='wrapper'>
+         <Filtration />
         <Routes>
           <Route path="/characters" Component={Characters} />
           <Route path='/characters/:id' Component={CharacterDetails} />
+          <Route path='/characters/:search' Component={FilteredCharacters} />
         </Routes>
        </div>
     </div>
---
+++ src/components/FilteredCharacters/FilteredCharacters.tsx
@@ -0,0 +1,5 @@
+export function FilteredCharacters () {
+	return (
+		<div>Привет</div>
+	)
+}
